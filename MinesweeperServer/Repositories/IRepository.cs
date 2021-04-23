ac96aa8e9c4a2a84df3fed47b6b66d0f220d06fd
ivan213k <ivan213k@gmail.com>
2021-04-23 07:57:56 +0300

    Added repositories

---
+++ MinesweeperServer/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MinesweeperServer.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(object id);
+        Task<List<TEntity>> GetAllAsync();
+        Task CreateAsync(TEntity item);
+        Task DeleteAsync(TEntity item);
+        Task UpdateAsync(TEntity item);
+    }
+}
---
+++ MinesweeperServer/Repositories/Repository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinesweeperServer.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MinesweeperServer.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        DbContext _dbContext;
+        DbSet<TEntity> _dbSet;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<TEntity>();
+        }
+        public async Task CreateAsync(TEntity item)
+        {
+            await _dbSet.AddAsync(item);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(TEntity item)
+        {
+            _dbSet.Remove(item);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(object id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(TEntity item)
+        {
+            _dbContext.Entry(item).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
